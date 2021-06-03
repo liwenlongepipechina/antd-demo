5ef5ec6f4ff90bb8d038bcca9af49ca3a9c816c6
liwenlong <wenlong.li>
2021-06-03 18:00:46 +0800

    增加：项目基础框架

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import '@/style/index.scss'
+
+import routes from './routes';
+import http from '@/utils/http.js';
+
+
+React.$http = http;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConfigProvider locale={zhCN}>
+    <HashRouter>
+      {renderRoutes(routes)}
+    </HashRouter>
+  </ConfigProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
